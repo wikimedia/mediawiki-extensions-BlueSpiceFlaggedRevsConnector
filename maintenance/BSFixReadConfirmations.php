6108c3a954da8bd2f6d7b21cf799366baa4d8f8b
akulbii <alk@astarios.com>
2021-03-24 16:19:14 +0200

    Update maintenance script
    Implemented "dry" mode for maintenance script.
    Updated it to extend correct Maintenance class instead of LoggedUpdateMaintenance.
    
    [3.2.x]
    
    ERM21033
    
    Change-Id: I1ebdd44e9f1408b7c0b997cd00e11865e1d22253

--- maintenance/BSFixReadConfirmations.php
+++ maintenance/BSFixReadConfirmations.php
@@ -8,216 +8,224 @@ use MediaWiki\Storage\RevisionLookup;
 use MediaWiki\Storage\RevisionRecord;
 use \Wikimedia\Rdbms\LoadBalancer;
 
-class BSFixReadConfirmations extends LoggedUpdateMaintenance {
-    /**
-     * Revision lookup instance.
-     * Used to search prev/next revisions in history, revisions by ID etc.
-     *
-     * @var RevisionLookup
-     */
-    private $revisionLookup;
+class BSFixReadConfirmations extends Maintenance {
+	/**
+	 * Revision lookup instance.
+	 * Used to search prev/next revisions in history, revisions by ID etc.
+	 *
+	 * @var RevisionLookup
+	 */
+	private $revisionLookup;
 
-    /**
-     * Load balancer instance.
-     * Used to interact with DB.
-     *
-     * @var LoadBalancer
-     */
-    private $loadBalancer;
+	/**
+	 * Load balancer instance.
+	 * Used to interact with DB.
+	 *
+	 * @var LoadBalancer
+	 */
+	private $loadBalancer;
 
-    /**
-     * Gets all revisions which are marked as read by some user.
-     *
-     * @return array Array with revisions, where key is revision ID and value is array, containing [userId => timestamp] key-values.
-     */
-    private function getUserReadRevisions() {
-        $res = $this->loadBalancer
-            ->getConnection( DB_REPLICA )
-            ->select(
-                'bs_readconfirmation',
-                '*',
-                [],
-                __METHOD__
-            );
+	/**
+	 * Adds "--dry" option to run script without actual changes in DB.
+	 */
+	public function __construct() {
+		parent::__construct();
+		$this->addOption( 'dry', 'Test run without doing actually DB fixes' );
+	}
 
-        $readRevisions = [];
-        foreach ( $res as $row ) {
-            $revId = (int) $row->rc_rev_id;
-            if ( !isset( $readRevisions[ $revId ] ) ) {
-                $readRevisions[ $revId ] = [];
-            }
-            $readRevisions[ $revId ][(int)$row->rc_user_id] = $row->rc_timestamp;
-        }
+	/**
+	 * Gets all revisions which are marked as read by some user.
+	 *
+	 * @return array Array with revisions, where key is revision ID and value is array, containing [userId => timestamp] key-values.
+	 */
+	private function getUserReadRevisions() {
+		$res = $this->loadBalancer
+			->getConnection( DB_REPLICA )
+			->select(
+				'bs_readconfirmation',
+				'*',
+				[],
+				__METHOD__
+			);
 
-        return $readRevisions;
-    }
+		$readRevisions = [];
+		foreach ( $res as $row ) {
+			$revId = (int) $row->rc_rev_id;
+			if ( !isset( $readRevisions[ $revId ] ) ) {
+				$readRevisions[ $revId ] = [];
+			}
+			$readRevisions[ $revId ][(int)$row->rc_user_id] = $row->rc_timestamp;
+		}
 
-    /**
-     * Checks whether specified revision is stable.
-     * It's considered as stable if is listed in "flaggedrevs" table and has "fr_quality > 0".
-     *
-     * @param int $revId Specified revision ID
-     * @return bool <tt>true</tt> if revision is stable, <tt>false</tt> otherwise.
-     */
-    private function isRevisionStable( $revId ) {
-        $res = $this->loadBalancer->getConnection( DB_REPLICA )->select(
-            'flaggedrevs',
-            [ 'fr_rev_id' ],
-            [
-                'fr_rev_id' => $revId,
-                'fr_quality > 0'
-            ],
-            __METHOD__,
-            [
-                'LIMIT' => 1
-            ]
-        );
-        if ( $res->numRows() > 0 ) {
-            return true;
-        }
-        return false;
-    }
+		return $readRevisions;
+	}
 
-    /**
-     * Gets specified revision and searches for nearest next MINOR STABLE revision in page revisions history.
-     *
-     * @param RevisionRecord $revision Specified revision's object.
-     * @return int|null Returns ID of necessary revision, or <tt>null</tt> if it was not found.
-     */
-    private function searchNextMinorStable( $revision ) {
-        while( true ) {
-            $nextRevision = $this->revisionLookup->getNextRevision( $revision );
+	/**
+	 * Checks whether specified revision is stable.
+	 * It's considered as stable if is listed in "flaggedrevs" table and has "fr_quality > 0".
+	 *
+	 * @param int $revId Specified revision ID
+	 * @return bool <tt>true</tt> if revision is stable, <tt>false</tt> otherwise.
+	 */
+	private function isRevisionStable( $revId ) {
+		$res = $this->loadBalancer->getConnection( DB_REPLICA )->select(
+			'flaggedrevs',
+			[ 'fr_rev_id' ],
+			[
+				'fr_rev_id' => $revId,
+				'fr_quality > 0'
+			],
+			__METHOD__,
+			[
+				'LIMIT' => 1
+			]
+		);
+		if ( $res->numRows() > 0 ) {
+			return true;
+		}
+		return false;
+	}
 
-            if( !is_null( $nextRevision ) ) {
-                $nextRevisionId = $nextRevision->getId();
+	/**
+	 * Gets specified revision and searches for nearest next MINOR STABLE revision in page revisions history.
+	 *
+	 * @param RevisionRecord $revision Specified revision's object.
+	 * @return int|null Returns ID of necessary revision, or <tt>null</tt> if it was not found.
+	 */
+	private function searchNextMinorStable( $revision ) {
+		while( true ) {
+			$nextRevision = $this->revisionLookup->getNextRevision( $revision );
 
-                if( $nextRevision->isMinor() && $this->isRevisionStable( $nextRevisionId ) ) {
-                    $this->output('MINOR STABLE found - ' . $nextRevisionId . "\n");
+			if( !is_null( $nextRevision ) ) {
+				$nextRevisionId = $nextRevision->getId();
 
-                    return $nextRevisionId;
-                }
-            }
-            else {
-                // End of revision history reached
-                return null;
-            }
+				if( $nextRevision->isMinor() && $this->isRevisionStable( $nextRevisionId ) ) {
+					$this->output('MINOR STABLE found - ' . $nextRevisionId . "\n");
 
-            $revision = $nextRevision;
-        }
-    }
+					return $nextRevisionId;
+				}
+			}
+			else {
+				// End of revision history reached
+				return null;
+			}
 
-    /**
-     * Gets specified revision and searches for nearest previous MAJOR STABLE revision in page revisions history.
-     *
-     * @param RevisionRecord $revision Specified revision's object.
-     * @return int|null Returns ID of necessary revision, or <tt>null</tt> if it was not found.
-     */
-    private function searchPrevMajorStable( $revision ) {
-        while( true ) {
-            $prevRevision = $this->revisionLookup->getPreviousRevision( $revision );
+			$revision = $nextRevision;
+		}
+	}
 
-            if( !is_null( $prevRevision ) ) {
-                $prevRevisionId = $prevRevision->getId();
+	/**
+	 * Gets specified revision and searches for nearest previous MAJOR STABLE revision in page revisions history.
+	 *
+	 * @param RevisionRecord $revision Specified revision's object.
+	 * @return int|null Returns ID of necessary revision, or <tt>null</tt> if it was not found.
+	 */
+	private function searchPrevMajorStable( $revision ) {
+		while( true ) {
+			$prevRevision = $this->revisionLookup->getPreviousRevision( $revision );
 
-                if( !$prevRevision->isMinor() && $this->isRevisionStable( $prevRevisionId ) ) {
-                    $this->output('MAJOR STABLE found - ' . $prevRevisionId . "\n");
+			if( !is_null( $prevRevision ) ) {
+				$prevRevisionId = $prevRevision->getId();
 
-                    return $prevRevisionId;
-                }
-            }
-            else {
-                // End of revision history reached
-                return null;
-            }
+				if( !$prevRevision->isMinor() && $this->isRevisionStable( $prevRevisionId ) ) {
+					$this->output('MAJOR STABLE found - ' . $prevRevisionId . "\n");
 
-            $revision = $prevRevision;
-        }
-    }
+					return $prevRevisionId;
+				}
+			}
+			else {
+				// End of revision history reached
+				return null;
+			}
 
-    /**
-     * Walks through all revisions, which have read confirmations.
-     *
-     * Deletes read confirmation entries for MINOR DRAFT revisions, if they are not based on MAJOR STABLE.
-     *
-     * In case of MAJOR DRAFT revisions, resets such read confirmation entries to nearest next MINOR STABLE.
-     * If there is not - deletes such entries.
-     *
-     * @return bool|void
-     */
-    protected function doDBUpdates() {
-        $this->loadBalancer = MediaWikiServices::getInstance()->getDBLoadBalancer();
-        $this->revisionLookup = MediaWikiServices::getInstance()->getRevisionLookup();
+			$revision = $prevRevision;
+		}
+	}
 
-        // Collect all read confirmations
-        $userReadRevisions = $this->getUserReadRevisions();
+	/**
+	 * Walks through all revisions, which have read confirmations.
+	 *
+	 * Deletes read confirmation entries for MINOR DRAFT revisions, if they are not based on MAJOR STABLE.
+	 *
+	 * In case of MAJOR DRAFT revisions, resets such read confirmation entries to nearest next MINOR STABLE.
+	 * If there is not - deletes such entries.
+	 *
+	 * @return bool|void
+	 */
+	public function execute() {
+		$this->loadBalancer = MediaWikiServices::getInstance()->getDBLoadBalancer();
+		$this->revisionLookup = MediaWikiServices::getInstance()->getRevisionLookup();
 
-        foreach( $userReadRevisions as $revId => $revisionReadConfirms ) {
-            $revision = $this->revisionLookup->getRevisionById( $revId );
+		// Collect all read confirmations
+		$userReadRevisions = $this->getUserReadRevisions();
 
-            $this->output('Current revision ID - ' . $revId . "\n");
+		foreach( $userReadRevisions as $revId => $revisionReadConfirms ) {
+			$revision = $this->revisionLookup->getRevisionById( $revId );
 
-            // If revision was MAJOR STABLE - do nothing
-            if( !$revision->isMinor() && $this->isRevisionStable( $revId ) ) {
-                $this->output('Revision is MAJOR STABLE' . "\n");
-                continue;
-            }
+			$this->output('Current revision ID - ' . $revId . "\n");
 
-            // Check if revision was MAJOR DRAFT
-            if( !$revision->isMinor() ) {
-                // Look up for MINOR STABLE among newer revisions of specified page
-                $this->output('Revision is MAJOR DRAFT' . "\n");
-                $this->output('Looking for MINOR STABLE among newer revisions...' . "\n");
+			// If revision was MAJOR STABLE - do nothing
+			if( !$revision->isMinor() && $this->isRevisionStable( $revId ) ) {
+				$this->output('Revision is MAJOR STABLE' . "\n");
+				$this->output('Nothing to do with it. Proceeding to the next one...' . "\n");
+				continue;
+			}
 
-                $newerMinorStableRevision = $this->searchNextMinorStable( $revision );
-                if( !is_null( $newerMinorStableRevision ) ) {
-                    // MINOR STABLE is found among newer revisions. So reset read confirmation entry to that MINOR STABLE revision
-                    $this->output('Reset read confirmation to specified MINOR STABLE revision' . "\n");
+			// Check if revision was MAJOR DRAFT
+			if( !$revision->isMinor() ) {
+				// Look up for MINOR STABLE among newer revisions of specified page
+				$this->output('Revision is MAJOR DRAFT' . "\n");
+				$this->output('Looking for MINOR STABLE among newer revisions...' . "\n");
 
-                    $this->loadBalancer->getConnection(DB_MASTER)->update(
-                        'bs_readconfirmation',
-                        ['rc_rev_id' => $newerMinorStableRevision],
-                        ['rc_rev_id' => $revId]
-                    );
-                }
-                else {
-                    // MINOR STABLE was not found among newer revisions. In such case we just delete specified read confirmation entry
-                    $this->output('Delete read confirmation entry for MAJOR DRAFT revision' . "\n");
+				$newerMinorStableRevision = $this->searchNextMinorStable( $revision );
+				if( !is_null( $newerMinorStableRevision ) ) {
+					// MINOR STABLE is found among newer revisions. So reset read confirmation entry to that MINOR STABLE revision
+					$this->output('Reset read confirmation to specified MINOR STABLE revision' . "\n");
 
-                    $this->loadBalancer->getConnection(DB_MASTER)->delete(
-                        'bs_readconfirmation',
-                        ['rc_rev_id' => $revId]
-                    );
-                }
-            }
-            // Otherwise revision was MINOR DRAFT
-            else {
-                $this->output('Revision is MINOR DRAFT' . "\n");
-                $this->output('Looking for MAJOR STABLE from later version (which current is based on)...' . "\n");
+					if( !$this->hasOption( 'dry' ) ) {
+						$this->loadBalancer->getConnection( DB_MASTER )->update(
+							'bs_readconfirmation',
+							['rc_rev_id' => $newerMinorStableRevision],
+							['rc_rev_id' => $revId]
+						);
+					}
+				}
+				else {
+					// MINOR STABLE was not found among newer revisions. In such case we just delete specified read confirmation entry
+					$this->output('Delete read confirmation entry for MAJOR DRAFT revision' . "\n");
 
-                // Check if it was based on MAJOR STABLE
-                $isBasedOnMajorStable = !!$this->searchPrevMajorStable( $revision );
+					if( !$this->hasOption( 'dry' ) ) {
+						$this->loadBalancer->getConnection( DB_MASTER )->delete(
+							'bs_readconfirmation',
+							['rc_rev_id' => $revId]
+						);
+					}
+				}
+			}
+			// Otherwise revision was MINOR DRAFT
+			else {
+				$this->output('Revision is MINOR DRAFT' . "\n");
+				$this->output('Looking for MAJOR STABLE from later version (which current is based on)...' . "\n");
 
-                // If MAJOR STABLE was not found - delete MINOR DRAFT entry
-                if( !$isBasedOnMajorStable ) {
-                    $this->output("Deleting of MINOR DRAFT entry...\n");
+				// Check if it was based on MAJOR STABLE
+				$isBasedOnMajorStable = !!$this->searchPrevMajorStable( $revision );
 
-                    $this->loadBalancer->getConnection(DB_MASTER)->delete(
-                        'bs_readconfirmation',
-                        ['rc_rev_id' => $revId]
-                    );
-                }
-            }
-        }
+				// If MAJOR STABLE was not found - delete MINOR DRAFT entry
+				if( !$isBasedOnMajorStable ) {
+					$this->output("Deleting of MINOR DRAFT entry...\n");
 
-        return true;
-    }
+					if( !$this->hasOption( 'dry' ) ) {
+						$this->loadBalancer->getConnection( DB_MASTER )->delete(
+							'bs_readconfirmation',
+							['rc_rev_id' => $revId]
+						);
+					}
+				}
+			}
+		}
 
-    /**
-     * @inheritDoc
-     */
-    protected function getUpdateKey() {
-        return 'bs_fix_read_confirmation_wrong_entries';
-    }
+		return true;
+	}
 }
 
 $maintClass = "BSFixReadConfirmations";
