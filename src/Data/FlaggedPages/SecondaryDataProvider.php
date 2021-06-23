389d1321a34cc16631785cac753e6b19ee3d6d3e
akulbii <alk@astarios.com>
2021-06-23 16:27:33 +0300

    Add 'category' column to quality management overview
    Also was changed links from category page to unreviewed pages and pending changes, which belong to specified category
    
    [3.2.x]
    
    ERM24070
    
    Change-Id: I1f3974c6bd854401b068db0c0a68f89b0213cb9a

--- src/Data/FlaggedPages/SecondaryDataProvider.php
+++ src/Data/FlaggedPages/SecondaryDataProvider.php
@@ -23,6 +23,20 @@ class SecondaryDataProvider implements \BlueSpice\Data\ISecondaryDataProvider {
 				$title
 			);
 			$record->set( Record::PAGE_LINK, $link );
+
+			$categoryLinks = [];
+
+			$categories = $record->get( Record::PAGE_CATEGORIES );
+			foreach ( $categories as $category ) {
+				$categoryTitle = \Title::newFromText( $category, NS_CATEGORY );
+				$link = MediaWikiServices::getInstance()->getLinkRenderer()->makeLink(
+					$categoryTitle,
+					$categoryTitle->getText()
+				);
+				$categoryLinks[] = $link;
+			}
+
+			$record->set( Record::PAGE_CATEGORIES_LINKS, $categoryLinks );
 		}
 
 		return $dataSets;
