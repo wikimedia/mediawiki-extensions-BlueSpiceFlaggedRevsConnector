6065d8da4241462f97704040f81119c267b0f1e5
miriam schlindwein <schlindwein@hallowelt.com>
2021-08-04 09:06:08 +0200

    Add extension to SkinSlot GlobalActionsManager
    [4.x]
    
    Change-Id: Iefae4868360fef13c25e894e211f08958388cb8d

---
+++ src/GlobalActionsManager.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace BlueSpice\FlaggedRevsConnector;
+
+use Message;
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\RestrictedTextLink;
+use SpecialPage;
+
+class GlobalActionsManager extends RestrictedTextLink {
+
+	/**
+	 *
+	 */
+	public function __construct() {
+		parent::__construct( [] );
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getId(): string {
+		return 'ga-bs-flaggedrevsconnector';
+	}
+
+	/**
+	 *
+	 * @return string[]
+	 */
+	public function getPermissions(): array {
+		$permissions = [
+			"review"
+		];
+		return $permissions;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHref(): string {
+		$tool = SpecialPage::getTitleFor( 'QMOverview' );
+		return $tool->getLocalURL();
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getText(): Message {
+		return Message::newFromKey( 'bs-flaggedrevsconnector-admin-tool-overview' );
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getTitle(): Message {
+		return Message::newFromKey( 'bs-qmoverview-desc' );
+	}
+
+	/**
+	 * @return Message
+	 */
+	public function getAriaLabel(): Message {
+		return Message::newFromKey( 'bs-flaggedrevsconnector-admin-tool-overview' );
+	}
+}
