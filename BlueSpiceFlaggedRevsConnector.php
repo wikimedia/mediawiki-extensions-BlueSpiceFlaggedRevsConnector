35e5630213527c2e623f8565e1a27b74a8aadd65
pwirth <wirth@hallowelt.biz>
2021-09-16 14:04:17 +0200

    Remove hook handler for long unused action menu
    The namespaces action menu is not used since 3.0.x
    
    ERM:10378
    [3.2.x]
    
    Change-Id: Ie03bafa208d720750cbb3de90087ca22e6cf9968

--- BlueSpiceFlaggedRevsConnector.php
+++ BlueSpiceFlaggedRevsConnector.php
@@ -185,11 +185,6 @@ class FlaggedRevsConnector extends Extension {
 		};
 	}
 
-	public static function setupFlaggedRevsConnector() {
-		global $wgHooks;
-		$wgHooks['SkinTemplateNavigation::Universal'][] = "FlaggedRevsConnector::onSkinTemplateNavigationUniversal";
-	}
-
 	protected $mFlagInfo = [];
 
 	/**
@@ -293,18 +288,4 @@ class FlaggedRevsConnector extends Extension {
 		return $aFlagInfo;
 	}
 
-	/**
-	 *
-	 * @param Skin $skin
-	 * @param array &$links
-	 * @return bool
-	 */
-	public static function onSkinTemplateNavigationUniversal( Skin $skin, array &$links ) {
-		if ( isset( $links['views']['current'] ) ) {
-			$links['namespaces']['current'] = $links['views']['current'];
-			$links['namespaces']['current']['text'] = wfMessage( "bs-flaggedrevsconnector-state-draft" )->plain();
-			unset( $links['views']['current'] );
-		}
-		return true;
-	}
 }
