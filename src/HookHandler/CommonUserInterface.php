45a257b93699585a377db8a553630df974137891
pwirth <wirth@hallowelt.biz>
2021-10-18 13:39:20 +0200

    Seperate userinterface hooks and fix naming
    ERM:23871
    [4.x]
    
    Change-Id: Ifae3b7204f8fe1dbf34c54bef509672c313880ee

--- src/HookHandler/CommonUserInterface.php
+++ src/HookHandler/CommonUserInterface.php
@@ -5,7 +5,7 @@ namespace BlueSpice\FlaggedRevsConnector\HookHandler;
 use BlueSpice\FlaggedRevsConnector\GlobalActionsManager;
 use MWStake\MediaWiki\Component\CommonUserInterface\Hook\MWStakeCommonUIRegisterSkinSlotComponents;
 
-class Main implements MWStakeCommonUIRegisterSkinSlotComponents {
+class CommonUserInterface implements MWStakeCommonUIRegisterSkinSlotComponents {
 
 	/**
 	 * @inheritDoc
@@ -15,7 +15,7 @@ class Main implements MWStakeCommonUIRegisterSkinSlotComponents {
 			'GlobalActionsManager',
 			[
 				'special-bluespice-flaggedrevsconnector' => [
-					'factory' => function () {
+					'factory' => static function () {
 						return new GlobalActionsManager();
 					}
 				]
