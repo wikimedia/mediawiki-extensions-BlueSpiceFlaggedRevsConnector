6da50648b08b0d353249545a610badffadc62920
d.savuljesku <d.savuljesku@gmail.com>
2022-02-24 16:27:58 +0100

    Show only stable drawio files
    [3.2.x]
    
    ERM 27738
    
    Change-Id: I48884c7ed01e988c3879548c51a0ba1cbc0ef92c

---
+++ src/HookHandler/GetStableFile.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace BlueSpice\FlaggedRevsConnector\HookHandler;
+
+use BlueSpice\FlaggedRevsConnector\Utils;
+use File;
+use MediaWiki\Extension\DrawioEditor\Hook\DrawioGetFileHook;
+use MediaWiki\Revision\RevisionStore;
+use User;
+
+class GetStableFile implements DrawioGetFileHook {
+	/** @var Utils */
+	private $utils;
+	/** @var RevisionStore */
+	private $revisionStore;
+
+	/**
+	 * @param Utils $utils
+	 * @param RevisionStore $revisionStore
+	 */
+	public function __construct( Utils $utils, RevisionStore $revisionStore ) {
+		$this->utils = $utils;
+		$this->revisionStore = $revisionStore;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onDrawioGetFile( File &$file, &$latestIsStable, User $user ) {
+		if ( !$this->utils->isFlaggableNamespace( $file->getTitle() ) ) {
+			return true;
+		}
+		if ( $this->utils->userCanAccessDrafts( $user ) ) {
+			return true;
+		}
+
+		$stable = $this->utils->getApprovedRevisionId( $file->getTitle() );
+		if ( !$stable ) {
+			$file = null;
+			$latestIsStable = false;
+			return true;
+		}
+
+		$stableRevision = $this->revisionStore->getRevisionById( $stable );
+		if ( !$stableRevision ) {
+			$file = null;
+			$latestIsStable = false;
+			return true;
+		}
+
+		if ( $stableRevision->isCurrent() ) {
+			$latestIsStable = true;
+			return true;
+		}
+
+		$oldFiles = $file->getHistory();
+		foreach ( $oldFiles as $oldFile ) {
+			if ( $oldFile->getTimestamp() === $stableRevision->getTimestamp() ) {
+				$file = $oldFile;
+				$latestIsStable = false;
+				return true;
+			}
+		}
+
+		return true;
+	}
+}
