1b12068bd9eb1f3bcc5cce612bfc814c61d5de46
miriam schlindwein <schlindwein@hallowelt.com>
2022-07-01 14:43:58 +0200

    Fix info about changed resources
    .. added popup button in alert to get information
    .. removed alert banner on stable version due to redundancy
    
    ERM27232
    
    [4.2.x]
    
    Change-Id: I75402ee9de87772e53fdca318676b3b8c9d17871

---
+++ resources/bluespice.flaggedrevsconnector.alert.js
@@ -0,0 +1,36 @@
+( function( mw, $ ) {
+
+	$( function () {
+		var $alert = $( '.alert.alert-warning' );
+
+		if ( $alert.length < 1 ) {
+			return;
+		}
+		var btn = OO.ui.infuse( '#bs-flagged-info-btn');
+
+		var infoBtn = new OO.ui.PopupButtonWidget( {
+			framed: false,
+			icon: 'info',
+			title: mw.message( 'bs-flaggedrevsconnector-state-draft-info-btn-title' ).text(),
+			popup: {
+				$content: getPopupContent( btn.data ),
+				padded: true,
+				align: 'force-left'
+			}
+		} );
+		$( '#bs-flagged-info-btn' ).html( infoBtn.$element );
+
+	} );
+
+	function getPopupContent ( data ) {
+		var $content = $( '<div>' );
+		$content.append( $( '<p>' ).text( mw.message( 'bs-flaggedrevsconnector-state-draft-info-btn-popup-title' ).text() ) );
+		var $list = $( '<ul>' ).addClass( 'bsfrc-files-list' );
+		for ( var item in data ) {
+			$list.append( $( '<li>' ).append( data[item] ) );
+		}
+
+		$content.append( $list );
+		return $content;
+	}
+})( mediaWiki, jQuery );
